2163c641fc83322e8c4d6563838eb7f69f5e6663
Marcley <marcleyrosa@gmail.com>
2022-12-04 10:37:42 -0300

    add middleware de errors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const { middleLogin } = require('./middlewares/verification');
 
 const { routerPost, routerUser, routerCategory } = require('./routers');
 
+require('express-async-errors');
+
 const app = express();
 
 app.use(express.json());
@@ -16,5 +18,14 @@ app.use('/post', routerPost);
 app.use('/categories', routerCategory);
 
 app.post('/login', middleLogin, postLogin);
+
+app.use((error, _req, _res, next) => {
+      console.error(error.stack);
+      next(error);
+});
+
+app.use((error, _req, res, _next) => {
+      res.status(500).send({ message: `Deu ruim ${error.message}` });
+});
 
 module.exports = app;
